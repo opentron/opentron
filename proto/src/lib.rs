6a942e103e18a323167049b766fe1105e8254b52
Shuyu WANG <andelf@gmail.com>
2019-12-24 11:26:19 +0800

    refactor(proto): run fmt

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,19 +1,18 @@
-extern crate protobuf;
-extern crate grpc;
 extern crate futures;
 extern crate futures_cpupool;
-
+extern crate grpc;
+extern crate protobuf;
 
 pub mod api;
 #[allow(unused_variables)]
 pub mod api_grpc;
 
 pub mod Contract;
-pub mod Tron;
 pub mod Discover;
+pub mod Tron;
 
 pub mod core {
     pub use super::Contract::*;
-    pub use super::Tron::*;
     pub use super::Discover::*;
+    pub use super::Tron::*;
 }
