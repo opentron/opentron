d0bb4a760b1682b5abbd2d69fd256cfa13681b99
Shuyu WANG <andelf@gmail.com>
2019-12-24 12:51:55 +0800

    feat: impl vote witness parsing

--- tron/commands/hello.rs
+++ tron/commands/hello.rs
@@ -1,5 +1,7 @@
 use clap::ArgMatches;
-use proto::core::Transaction_raw;
+use proto::core::{
+    Transaction_Contract_ContractType as ContractType, Transaction_raw as TransactionRaw, VoteWitnessContract,
+};
 use protobuf::parse_from_bytes;
 
 pub fn run(matches: &ArgMatches) -> Result<(), String> {
@@ -8,9 +10,20 @@ pub fn run(matches: &ArgMatches) -> Result<(), String> {
     println!("HASH = {:?}", hash);
 
     let raw = hex::decode(hash).expect("hex decode ok");
-    let tx = parse_from_bytes::<Transaction_raw>(&raw).expect("parse ok");
+    let tx = parse_from_bytes::<TransactionRaw>(&raw).expect("parse ok");
 
-    println!("{:?}", tx);
+    for contr in &tx.contract {
+        println!("got {:?}", contr.field_type);
+        match contr.field_type {
+            ContractType::VoteWitnessContract => {
+                let param = contr.parameter.as_ref().map(|p| &p.value).expect("parameter body");
+                let vote_witness =
+                    parse_from_bytes::<VoteWitnessContract>(param).expect("pb VoteWitnessContract error");
+                println!("param = {:?}", vote_witness);
+            }
+            _ => (),
+        }
+    }
 
     Ok(())
 }
