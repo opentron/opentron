c6fe111f022de5a956a7bdf45897d28b2439c678
Shuyu WANG <andelf@gmail.com>
2019-12-27 17:09:36 +0800

    fix(wallet-cli): error printing usage

--- wallet-cli/src/main.rs
+++ wallet-cli/src/main.rs
@@ -13,11 +13,11 @@ fn main() -> Result<(), String> {
     let yaml = load_yaml!("cli.yml");
     let matches = clap::App::from_yaml(yaml).get_matches();
 
-    // println!("DEBUG matches: {:?}", matches);
-    // println!("");
-
     match matches.subcommand() {
         ("get", Some(import_matches)) => commands::get::main(import_matches),
-        _ => Err("error parsing command line".to_owned()),
+        _ => {
+            println!("{}", matches.usage());
+            Err("error parsing command line".to_owned())
+        }
     }
 }
