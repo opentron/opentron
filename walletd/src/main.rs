a208a4602a0cc75b1a937eee0ae5c7f6372c1af2
Shuyu WANG <andelf@gmail.com>
2019-12-30 19:20:17 +0800

    feat(walletd): introduce tonic

--- walletd/src/main.rs
+++ walletd/src/main.rs
@@ -1,3 +1,12 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use api::wallet_server::{Wallet, WalletServer};
+use api::{CreateRequest, StatusResponse};
+
+pub mod api {
+    tonic::include_proto!("network.tron.walletd");
+}
+
 fn main() {
     println!("Hello, world!");
 }
