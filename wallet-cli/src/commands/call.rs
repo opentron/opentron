a00648dfbd97c5a54b039efd1451b5d856cad837
andelf <andelf@gmail.com>
2020-01-07 01:56:37 +0800

    refactor(wallet-cli): use TransactionHandler in contract Call
    - Fix missing arg count check

--- wallet-cli/src/commands/call.rs
+++ wallet-cli/src/commands/call.rs
@@ -3,15 +3,11 @@
 use clap::ArgMatches;
 use hex::{FromHex, ToHex};
 use keys::Address;
-use proto::api_grpc::Wallet;
 use proto::core::TriggerSmartContract;
-use protobuf::Message;
-use serde_json::json;
 
 use crate::error::Error;
 use crate::utils::abi;
-use crate::utils::client;
-use crate::utils::jsont;
+use crate::utils::trx;
 
 pub fn main(matches: &ArgMatches) -> Result<(), Error> {
     let sender = matches
@@ -27,6 +23,9 @@ pub fn main(matches: &ArgMatches) -> Result<(), Error> {
     let data = match (matches.values_of("ARGS"), matches.value_of("data")) {
         (Some(args), None) => {
             let types = extract_types(method)?;
+            if matches.occurrences_of("ARGS") as usize != types.len() {
+                return Err(Error::Runtime("wrong number of ARGS"));
+            }
             // Fix tron base58checked addresses, remove 0x41
             let values = args
                 .zip(types.iter())
@@ -48,7 +47,7 @@ pub fn main(matches: &ArgMatches) -> Result<(), Error> {
         (None, Some(data_hex)) => Vec::from_hex(data_hex)?,
         // nullary call
         (None, None) => Vec::from(&abi::fnhash(method)[..]),
-        (_, _) => unreachable!("set conflicts in cli.yml; qed")
+        (_, _) => unreachable!("set conflicts in cli.yml; qed"),
     };
 
     let trigger_contract = TriggerSmartContract {
@@ -58,33 +57,9 @@ pub fn main(matches: &ArgMatches) -> Result<(), Error> {
         ..Default::default()
     };
 
-    // creating transaction
-    let (_, mut transaction_ext, _) = client::new_grpc_client()?
-        .trigger_contract(Default::default(), trigger_contract)
-        .wait()?;
-
-    // MUST fix fee_limit, or OUT_OF_ENERGY
-    transaction_ext
-        .mut_transaction()
-        .mut_raw_data()
-        .set_fee_limit(1_000_000);
-
-    let mut json = serde_json::to_value(&transaction_ext)?;
-    jsont::fix_transaction_ext(&mut json)?;
-
-    if json["result"]["result"].as_bool().unwrap_or(false) {
-        json["transaction"]["raw_data_hex"] = json!(transaction_ext
-            .get_transaction()
-            .get_raw_data()
-            .write_to_bytes()?
-            .encode_hex::<String>());
-
-        println!("{}", serde_json::to_string_pretty(&json["transaction"])?);
-        Ok(())
-    } else {
-        eprintln!("{}", serde_json::to_string_pretty(&json)?);
-        Err(Error::Runtime("can not create transaction"))
-    }
+    trx::TransactionHandler::handle(trigger_contract, matches)
+        .map_raw_transaction(|raw| raw.set_fee_limit(1_000_000))
+        .run()
 }
 
 #[inline]
