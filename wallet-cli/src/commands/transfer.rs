61dcc901a20d05704541a15771079cf0ec999157
andelf <andelf@gmail.com>
2020-01-07 00:56:46 +0800

    refactor(wallet-cli): use TransactionHandler to handle args

--- wallet-cli/src/commands/transfer.rs
+++ wallet-cli/src/commands/transfer.rs
@@ -1,23 +1,12 @@
 use clap::ArgMatches;
-use hex::ToHex;
-use keys::{Address, Private};
-use proto::api_grpc::Wallet;
-use proto::core::{
-    Transaction, Transaction_Contract as Contract, Transaction_Contract_ContractType as ContractType,
-    Transaction_raw as TransactionRaw, TransferContract,
-};
-use protobuf::well_known_types::Any;
-use protobuf::Message;
-use serde_json::json;
+use keys::Address;
+use proto::core::TransferContract;
 
-use crate::commands::wallet::sign_digest;
 use crate::error::Error;
-use crate::utils::client;
-use crate::utils::crypto;
-use crate::utils::jsont;
 use crate::utils::trx;
+use crate::utils::trx::TransactionHandler;
 
-pub fn main(matches: &ArgMatches) -> Result<(), Error> {
+pub fn main<'a>(matches: &'a ArgMatches<'a>) -> Result<(), Error> {
     let sender = matches
         .value_of("SENDER")
         .and_then(|s| s.parse::<Address>().ok())
@@ -27,92 +16,19 @@ pub fn main(matches: &ArgMatches) -> Result<(), Error> {
         .and_then(|s| s.parse::<Address>().ok())
         .ok_or(Error::Runtime("wrong recipient address format"))?;
     let amount = matches.value_of("AMOUNT").expect("required in cli.yml; qed");
-    let memo = matches.value_of("MEMO").unwrap_or("");
+    let memo = matches.value_of("MEMO").unwrap_or("").as_bytes().to_owned();
 
-    let client = client::new_grpc_client()?;
-
-    let trx_contract = TransferContract {
+    let transfer_contract = TransferContract {
         owner_address: sender.to_bytes().to_owned(),
         to_address: recipient.to_bytes().to_owned(),
         amount: trx::parse_amount(amount, true)?,
         ..Default::default()
     };
 
-    // packing contract
-    let mut any = Any::new();
-    any.set_type_url("type.googleapis.com/protocol.TransferContract".to_owned());
-    any.set_value(trx_contract.write_to_bytes()?);
-
-    let mut contract = Contract::new();
-    contract.set_field_type(ContractType::TransferContract);
-    contract.set_parameter(any);
-    if let Some(val) = matches.value_of("permission-id") {
-        contract.set_Permission_id(val.parse()?);
-    }
-
-    let mut raw = TransactionRaw::new();
-    raw.set_contract(vec![contract].into());
-    raw.set_data(memo.into());
-
-    let expiration = matches
-        .value_of("expiration")
-        .expect("has default value in cli.yml; qed")
-        .parse::<i64>()?;
-    raw.set_expiration(trx::timestamp_millis() + 1000 * expiration);
-
-    // fill ref_block info
-    let ref_block = client::get_latest_block(&client)?;
-    let ref_block_number = ref_block.get_block_header().get_raw_data().number;
-    raw.set_ref_block_bytes(vec![
-        ((ref_block_number & 0xff00) >> 8) as u8,
-        (ref_block_number & 0xff) as u8,
-    ]);
-    raw.set_ref_block_hash(ref_block.blockid[8..16].to_owned());
-    raw.set_timestamp(trx::timestamp_millis());
-
-    let txid = crypto::sha256(&raw.write_to_bytes()?);
-
-    // signature
-    let mut signatures: Vec<Vec<u8>> = Vec::new();
-    if !matches.is_present("skip-sign") {
-        let signature = if let Some(raw_addr) = matches.value_of("account") {
-            let addr = raw_addr.parse::<Address>()?;
-            eprintln!("! Signing using wallet key from --account {:}", addr);
-            sign_digest(&txid, &addr)?
-        } else if let Some(raw_key) = matches.value_of("private-key") {
-            eprintln!("! Signing using raw private key from --private-key");
-            let priv_key = raw_key.parse::<Private>()?;
-            priv_key.sign_digest(&txid)?[..].to_owned()
-        } else {
-            eprintln!("! Signing using wallet key {:}", sender);
-            sign_digest(&txid, &sender)?
-        };
-        signatures.push(signature);
-    }
-
-    let mut req = Transaction::new();
-    req.set_raw_data(raw);
-    req.set_signature(signatures.into());
-
     eprintln!("sender:    {:}", sender);
     eprintln!("recipient: {:}", recipient);
-    eprintln!("TX: {:}", txid.encode_hex::<String>());
 
-    // skip-sign implies dont-broadcast
-    if matches.is_present("skip-sign") || matches.is_present("dont-broadcast") {
-        let mut json = serde_json::to_value(&req)?;
-        jsont::fix_transaction(&mut json)?;
-        json["raw_data_hex"] = json!(req.get_raw_data().write_to_bytes()?.encode_hex::<String>());
-        json["txID"] = json!(txid.encode_hex::<String>());
-        println!("{:}", serde_json::to_string_pretty(&json)?);
-    } else {
-        let (_, payload, _) = client.broadcast_transaction(Default::default(), req).wait()?;
-
-        let mut result = serde_json::to_value(&payload)?;
-        jsont::fix_api_return(&mut result);
-
-        eprintln!("got => {:}", serde_json::to_string_pretty(&result)?);
-    }
-
-    Ok(())
+    TransactionHandler::handle(transfer_contract, matches)
+        .map_raw_transaction(move |raw| raw.set_data(memo.clone()))
+        .run()
 }
--- wallet-cli/src/utils/trx.rs
+++ wallet-cli/src/utils/trx.rs
@@ -1,15 +1,33 @@
 //! Helpers for transaction.
 
 use chrono::Utc;
-use keys::Address;
+use clap::ArgMatches;
+use hex::ToHex;
+use keys::{Address, Private};
+use proto::api_grpc::Wallet;
 use proto::core::{
-    CreateSmartContract, FreezeBalanceContract, TransferContract, TriggerSmartContract, UnfreezeBalanceContract
+    AccountCreateContract, AccountPermissionUpdateContract, AccountUpdateContract, AssetIssueContract,
+    ClearABIContract, CreateSmartContract, ExchangeCreateContract, ExchangeInjectContract, ExchangeTransactionContract,
+    ExchangeWithdrawContract, FreezeBalanceContract, ParticipateAssetIssueContract, ProposalApproveContract,
+    ProposalCreateContract, ProposalDeleteContract, SetAccountIdContract, ShieldedTransferContract,
+    TransferAssetContract, TransferContract, TriggerSmartContract, UnfreezeAssetContract, UnfreezeBalanceContract,
+    UpdateAssetContract, UpdateBrokerageContract, UpdateEnergyLimitContract, UpdateSettingContract, VoteAssetContract,
+    VoteWitnessContract, WithdrawBalanceContract, WitnessCreateContract, WitnessUpdateContract,
 };
-use protobuf::parse_from_bytes;
+use proto::core::{
+    Transaction, Transaction_Contract as Contract, Transaction_Contract_ContractType as ContractType,
+    Transaction_raw as TransactionRaw,
+};
 use protobuf::well_known_types::Any;
+use protobuf::{parse_from_bytes, Message};
+use serde_json::json;
 use std::convert::TryFrom;
 
+use crate::commands::wallet::sign_digest;
 use crate::error::Error;
+use crate::utils::client;
+use crate::utils::crypto;
+use crate::utils::jsont;
 
 pub fn timestamp_millis() -> i64 {
     Utc::now().timestamp_millis()
@@ -18,24 +36,24 @@ pub fn timestamp_millis() -> i64 {
 pub fn extract_owner_address_from_parameter(any: &Any) -> Result<Address, Error> {
     match any.get_type_url() {
         "type.googleapis.com/protocol.TransferContract" => Ok(Address::try_from(
-            parse_from_bytes::<TransferContract>(any.get_value())?.get_owner_address()
+            parse_from_bytes::<TransferContract>(any.get_value())?.get_owner_address(),
         )?),
         "type.googleapis.com/protocol.ShieldedTransferContract" => Err(Error::Runtime(
-            "can not extract sender address from ShieldedTransferContract. Use -k/-K instead."
+            "can not extract sender address from ShieldedTransferContract. Use -k/-K instead.",
         )),
         "type.googleapis.com/protocol.CreateSmartContract" => Ok(Address::try_from(
-            parse_from_bytes::<CreateSmartContract>(any.get_value())?.get_owner_address()
+            parse_from_bytes::<CreateSmartContract>(any.get_value())?.get_owner_address(),
         )?),
         "type.googleapis.com/protocol.TriggerSmartContract" => Ok(Address::try_from(
-            parse_from_bytes::<TriggerSmartContract>(any.get_value())?.get_owner_address()
+            parse_from_bytes::<TriggerSmartContract>(any.get_value())?.get_owner_address(),
         )?),
         "type.googleapis.com/protocol.FreezeBalanceContract" => Ok(Address::try_from(
-            parse_from_bytes::<FreezeBalanceContract>(any.get_value())?.get_owner_address()
+            parse_from_bytes::<FreezeBalanceContract>(any.get_value())?.get_owner_address(),
         )?),
         "type.googleapis.com/protocol.UnfreezeBalanceContract" => Ok(Address::try_from(
-            parse_from_bytes::<UnfreezeBalanceContract>(any.get_value())?.get_owner_address()
+            parse_from_bytes::<UnfreezeBalanceContract>(any.get_value())?.get_owner_address(),
         )?),
-        _ => unimplemented!()
+        _ => unimplemented!(),
     }
 }
 
@@ -49,10 +67,11 @@ pub fn parse_amount(amount: &str, allow_unit: bool) -> Result<i64, Error> {
         let length = amount.as_bytes().len();
         // FIXME: allow other unit
         if amount.ends_with("TRX") || amount.ends_with("TRZ") {
-            Ok(String::from_utf8_lossy(&amount.as_bytes()[..length - 3])
+            String::from_utf8_lossy(&amount.as_bytes()[..length - 3])
                 .replace("_", "")
-                .parse::<i64>()? *
-                1_000_000)
+                .parse::<i64>()
+                .map(|v| v * 1_000_000)
+                .map_err(Error::from)
         } else if amount.ends_with("SUN") {
             Ok(String::from_utf8_lossy(&amount.as_bytes()[..length - 3])
                 .replace("_", "")
@@ -64,3 +83,165 @@ pub fn parse_amount(amount: &str, allow_unit: bool) -> Result<i64, Error> {
         Ok(amount.replace("_", "").parse()?)
     }
 }
+
+pub struct TransactionHandler<'a, C> {
+    contract: C,
+    arg_matches: &'a ArgMatches<'a>,
+    raw_trx_fn: Option<Box<dyn FnMut(&mut TransactionRaw) -> () + 'static>>,
+}
+
+impl<'a, C: ContractPbExt> TransactionHandler<'a, C> {
+    pub fn handle(contract: C, matches: &'a ArgMatches<'a>) -> Self {
+        TransactionHandler {
+            contract,
+            arg_matches: matches,
+            raw_trx_fn: None,
+        }
+    }
+
+    pub fn map_raw_transaction<F>(&mut self, f: F) -> &mut Self
+    where
+        F: FnMut(&mut TransactionRaw) -> () + 'static,
+    {
+        self.raw_trx_fn = Some(Box::new(f));
+        self
+    }
+
+    pub fn run(&mut self) -> Result<(), Error> {
+        let matches = self.arg_matches;
+
+        // packing contract
+        let any = self.contract.as_google_any()?;
+
+        let mut contract = Contract::new();
+        contract.set_field_type(self.contract.contract_type());
+        contract.set_parameter(any);
+        if let Some(val) = matches.value_of("permission-id") {
+            contract.set_Permission_id(val.parse()?);
+        }
+
+        let mut raw = TransactionRaw::new();
+        raw.set_contract(vec![contract].into());
+        if let Some(f) = self.raw_trx_fn.as_mut() {
+            f(&mut raw);
+        }
+
+        let expiration = matches
+            .value_of("expiration")
+            .expect("has default value in cli.yml; qed")
+            .parse::<i64>()?;
+        raw.set_expiration(timestamp_millis() + 1000 * expiration);
+
+        let grpc_client = client::new_grpc_client()?;
+
+        // fill ref_block info
+        let ref_block = client::get_latest_block(&grpc_client)?;
+        let ref_block_number = ref_block.get_block_header().get_raw_data().number;
+        raw.set_ref_block_bytes(vec![
+            ((ref_block_number & 0xff00) >> 8) as u8,
+            (ref_block_number & 0xff) as u8,
+        ]);
+        raw.set_ref_block_hash(ref_block.blockid[8..16].to_owned());
+        raw.set_timestamp(timestamp_millis());
+
+        let txid = crypto::sha256(&raw.write_to_bytes()?);
+
+        // signature
+        let mut signatures: Vec<Vec<u8>> = Vec::new();
+        if !matches.is_present("skip-sign") {
+            let signature = if let Some(raw_addr) = matches.value_of("account") {
+                let addr = raw_addr.parse::<Address>()?;
+                eprintln!("! Signing using wallet key from --account {:}", addr);
+                sign_digest(&txid, &addr)?
+            } else if let Some(raw_key) = matches.value_of("private-key") {
+                eprintln!("! Signing using raw private key from --private-key");
+                let priv_key = raw_key.parse::<Private>()?;
+                priv_key.sign_digest(&txid)?[..].to_owned()
+            } else {
+                let owner_address = extract_owner_address_from_parameter(raw.contract[0].get_parameter())?;
+                eprintln!("! Signing using wallet key {:}", owner_address);
+                sign_digest(&txid, &owner_address)?
+            };
+            signatures.push(signature);
+        }
+
+        let mut req = Transaction::new();
+        req.set_raw_data(raw);
+        req.set_signature(signatures.into());
+
+        eprintln!("TX: {:}", txid.encode_hex::<String>());
+
+        // skip-sign implies dont-broadcast
+        if matches.is_present("skip-sign") || matches.is_present("dont-broadcast") {
+            let mut json = serde_json::to_value(&req)?;
+            jsont::fix_transaction(&mut json)?;
+            json["raw_data_hex"] = json!(req.get_raw_data().write_to_bytes()?.encode_hex::<String>());
+            json["txID"] = json!(txid.encode_hex::<String>());
+            println!("{:}", serde_json::to_string_pretty(&json)?);
+        } else {
+            let (_, payload, _) = grpc_client.broadcast_transaction(Default::default(), req).wait()?;
+
+            let mut result = serde_json::to_value(&payload)?;
+            jsont::fix_api_return(&mut result);
+            eprintln!("got => {:}", serde_json::to_string_pretty(&result)?);
+        }
+
+        Ok(())
+    }
+}
+
+/// Helper trait for packing contract.
+pub trait ContractPbExt: Message {
+    fn contract_type(&self) -> ContractType;
+
+    /// Convert Pb to protobuf::well_known_types::Any
+    fn as_google_any(&self) -> Result<Any, protobuf::ProtobufError> {
+        Ok(Any {
+            type_url: format!("type.googleapis.com/protocol.{:?}", self.contract_type()),
+            value: self.write_to_bytes()?,
+            ..Default::default()
+        })
+    }
+}
+
+macro_rules! impl_contract_pb_ext_for {
+    ($contract_ty:ident) => {
+        impl ContractPbExt for $contract_ty {
+            fn contract_type(&self) -> ContractType {
+                ContractType::$contract_ty
+            }
+        }
+    };
+}
+
+impl_contract_pb_ext_for!(AccountCreateContract);
+impl_contract_pb_ext_for!(TransferContract);
+impl_contract_pb_ext_for!(TransferAssetContract);
+impl_contract_pb_ext_for!(VoteAssetContract);
+impl_contract_pb_ext_for!(VoteWitnessContract);
+impl_contract_pb_ext_for!(WitnessCreateContract);
+impl_contract_pb_ext_for!(AssetIssueContract);
+impl_contract_pb_ext_for!(WitnessUpdateContract);
+impl_contract_pb_ext_for!(ParticipateAssetIssueContract);
+impl_contract_pb_ext_for!(AccountUpdateContract);
+impl_contract_pb_ext_for!(FreezeBalanceContract);
+impl_contract_pb_ext_for!(UnfreezeBalanceContract);
+impl_contract_pb_ext_for!(WithdrawBalanceContract);
+impl_contract_pb_ext_for!(UnfreezeAssetContract);
+impl_contract_pb_ext_for!(UpdateAssetContract);
+impl_contract_pb_ext_for!(ProposalCreateContract);
+impl_contract_pb_ext_for!(ProposalApproveContract);
+impl_contract_pb_ext_for!(ProposalDeleteContract);
+impl_contract_pb_ext_for!(SetAccountIdContract);
+impl_contract_pb_ext_for!(CreateSmartContract);
+impl_contract_pb_ext_for!(TriggerSmartContract);
+impl_contract_pb_ext_for!(UpdateSettingContract);
+impl_contract_pb_ext_for!(ExchangeCreateContract);
+impl_contract_pb_ext_for!(ExchangeInjectContract);
+impl_contract_pb_ext_for!(ExchangeWithdrawContract);
+impl_contract_pb_ext_for!(ExchangeTransactionContract);
+impl_contract_pb_ext_for!(UpdateEnergyLimitContract);
+impl_contract_pb_ext_for!(AccountPermissionUpdateContract);
+impl_contract_pb_ext_for!(ClearABIContract);
+impl_contract_pb_ext_for!(UpdateBrokerageContract);
+impl_contract_pb_ext_for!(ShieldedTransferContract);
