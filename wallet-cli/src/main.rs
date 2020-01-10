19586d6a4432c09de2483bb99158dc7466414e12
andelf <andelf@gmail.com>
2020-01-10 22:01:05 +0800

    feat(wallet-cli): impl rpc-host option

--- wallet-cli/src/main.rs
+++ wallet-cli/src/main.rs
@@ -15,15 +15,15 @@ fn main() -> Result<(), Error> {
     let yaml = load_yaml!("cli.yml");
     let matches = clap::App::from_yaml(yaml).get_matches();
 
-    // FIXME: as above
     unsafe {
-        RPC_HOST = match matches.value_of("network") {
-            Some("mainnet") => "grpc.trongrid.io:50051",
-            Some("shasta") => "grpc.shasta.trongrid.io:50051",
-            Some("nile") => "47.252.3.238:50051",
-            Some("testnet") => "47.252.87.110:50051",
-            Some("dappchain") => "47.90.245.159:50051",
-            Some("dappchain-testnet") => "47.252.85.90:50051",
+        RPC_HOST = match (matches.value_of("network"), matches.value_of("rpc-host")) {
+            (_, Some(host)) => host,
+            (Some("mainnet"), _) => "grpc.trongrid.io:50051",
+            (Some("shasta"), _) => "grpc.shasta.trongrid.io:50051",
+            (Some("nile"), _) => "47.252.3.238:50051",
+            (Some("testnet"), _) => "47.252.87.110:50051",
+            (Some("dappchain"), _) => "47.90.245.159:50051",
+            (Some("dappchain-testnet"), _) => "47.252.85.90:50051",
             _ => unreachable!(),
         }
     }
