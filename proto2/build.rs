20b548b10835a9f4d8e901182d4dfeead04c9f26
andelf <andelf@gmail.com>
2020-03-19 18:32:35 +0800

    refactor(proto2): rename discover to discovery

--- proto2/build.rs
+++ proto2/build.rs
@@ -2,7 +2,7 @@ fn main() {
     prost_build::compile_protos(
         &[
             "proto/common.proto",
-            "proto/discover.proto",
+            "proto/discovery.proto",
             "proto/chain.proto",
             "proto/channel.proto",
             "proto/contract.proto",
--- proto2/src/lib.rs
+++ proto2/src/lib.rs
@@ -4,10 +4,20 @@ pub mod common {
 
 pub mod chain {
     include!(concat!(env!("OUT_DIR"), "/proto.chain.rs"));
+
+    impl ::std::fmt::Display for Block {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+            let raw_header = &self.block_header.as_ref().unwrap().raw_data.as_ref().unwrap();
+            f.debug_struct("Block")
+                .field("number", &raw_header.number)
+                .field("txns", &self.transactions.len())
+                .finish()
+        }
+    }
 }
 
-pub mod discover {
-    include!(concat!(env!("OUT_DIR"), "/proto.discover.rs"));
+pub mod discovery {
+    include!(concat!(env!("OUT_DIR"), "/proto.discovery.rs"));
 }
 
 pub mod channel {
