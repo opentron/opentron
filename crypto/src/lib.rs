ddbc26eae0cadb7b09e77080f56add5bcf0f0441
andelf <andelf@gmail.com>
2020-03-20 17:33:38 +0800

    feat(crypto): add crypto crate

---
+++ crypto/src/lib.rs
@@ -0,0 +1,20 @@
+use digest::Digest;
+use primitives::H256;
+use sha2::Sha256;
+use sha3::Keccak256;
+
+#[inline]
+pub fn sha256(input: &[u8]) -> H256 {
+    let mut hasher = Sha256::new();
+    hasher.input(input);
+    let inner: [u8; 32] = hasher.result().into();
+    H256::from(inner)
+}
+
+#[inline]
+pub fn keccak256(input: &[u8]) -> H256 {
+    let mut hasher = Keccak256::new();
+    hasher.input(input);
+    let inner: [u8; 32] = hasher.result().into();
+    H256::from(inner)
+}
