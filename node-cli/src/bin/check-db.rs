bdefdfdb35725b8ac1060485ccfcd36e94455bfd
andelf <andelf@gmail.com>
2020-05-17 19:11:43 +0800

    feat(node): compact db before check-db

--- node-cli/src/bin/check-db.rs
+++ node-cli/src/bin/check-db.rs
@@ -6,7 +6,15 @@ use node_cli::db::ChainDB;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::load_from_file("./conf.toml")?;
     let db = ChainDB::new(&config.storage.data_dir);
+    println!("db opened");
 
+    println!("compact db ...");
+    let ret = db.compact_db();
+    println!("compact => {:?}", ret);
+
+    db.await_background_jobs();
+
+    db.verify_parent_hashes()?;
     db.verify_merkle_tree()?;
 
     Ok(())
