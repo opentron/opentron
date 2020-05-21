31358d0ce1d2d40c1df49443185b5039808888f7
andelf <andelf@gmail.com>
2020-05-21 14:40:39 +0800

    feat(proto2): enable derive for SmartContract::ABI

--- proto2/build.rs
+++ proto2/build.rs
@@ -1,13 +1,17 @@
 fn main() {
-    prost_build::compile_protos(
-        &[
-            "proto/common.proto",
-            "proto/discovery.proto",
-            "proto/chain.proto",
-            "proto/channel.proto",
-            "proto/contract.proto",
-        ],
-        &["proto/"],
-    )
-    .unwrap();
+    prost_build::Config::new()
+        .type_attribute("proto.common.SmartContract.ABI", "#[derive(serde::Serialize)]")
+        .type_attribute("proto.common.SmartContract.ABI.Entry", "#[derive(serde::Serialize)]")
+        .type_attribute("proto.common.SmartContract.ABI.Param", "#[derive(serde::Serialize)]")
+        .compile_protos(
+            &[
+                "proto/common.proto",
+                "proto/discovery.proto",
+                "proto/chain.proto",
+                "proto/channel.proto",
+                "proto/contract.proto",
+            ],
+            &["proto/"],
+        )
+        .unwrap();
 }
