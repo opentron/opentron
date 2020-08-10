097851a47ae7cf334ebeefa6dd8ee7be8390c1e6
andelf <andelf@gmail.com>
2020-08-10 20:49:12 +0800

    feat(node/manager): impl state db manager
    - A StateDB manger
    - TransferContract handler
    - Maintenance handler
    - Bandwidth processor

--- opentron/src/commands/dev.rs
+++ opentron/src/commands/dev.rs
@@ -2,26 +2,39 @@ use clap::ArgMatches;
 use log::info;
 use std::path::Path;
 
-// use crate::config::Config;
-// use crate::db::ChainDB;
-use state::db::StateDB;
 use crate::context::AppContext;
+use crate::manager::Manager;
+use chrono::Utc;
 
 pub async fn main<P: AsRef<Path>>(config_path: P, _matches: &ArgMatches<'_>) -> Result<(), Box<dyn std::error::Error>> {
-    // let config = Config::load_from_file(config_path)?;
     let ctx = AppContext::from_config(config_path)?;
-    info!("config file loaded");
-    // info!("{:#?}", ctx.config);
-    let mut state_db = StateDB::new(&ctx.config.storage.state_data_dir);
-    info!("state db opened");
-    // info!("genesis config => {:#?}", ctx.genesis_config);
 
-    state_db.init_genesis(&ctx.genesis_config, &ctx.config.chain)?;
+    let mut db_manager = Manager::new(&ctx.config, &ctx.genesis_config);
 
-    for i in 1..10 {
+    let ref_block_hashes = ctx
+        .chain_db
+        .ref_block_hashes_of_block_num(db_manager.latest_block_number());
+    /*for (i, v) in ref_block_hashes.iter().enumerate() {
+        println!("{} {:x}=> {:?}", i, i, v);
+    }*/
+    db_manager.init_ref_blocks(ref_block_hashes);
+
+    let mut start_time = Utc::now().timestamp_millis();
+    let mut n_blocks = 0;
+
+    for i in 1..30000 {
+        // for i in 28514..30000 {
         let blk = ctx.chain_db.get_block_by_number(i)?;
 
-       // state_db.apply_block(&blk);
+        db_manager.push_block(&blk)?;
+
+        n_blocks += 1;
+
+        if n_blocks % 1_000 == 0 {
+            let now = Utc::now().timestamp_millis();
+            info!("speed => {}blocks/s", 1_000 * 1_000 / (now - start_time));
+            start_time = Utc::now().timestamp_millis();
+        }
     }
 
     Ok(())
---
+++ opentron/src/manager/maintenance.rs
@@ -0,0 +1,96 @@
+use ::keys::Address;
+use chain::IndexedBlock;
+use log::debug;
+use state::keys;
+
+use super::Manager;
+
+pub struct MaintenanceManager<'m> {
+    manager: &'m mut Manager,
+}
+
+impl MaintenanceManager<'_> {
+    pub fn new<'a>(manager: &'a mut Manager) -> MaintenanceManager<'a> {
+        MaintenanceManager { manager }
+    }
+
+    pub fn apply_block(mut self, block: &IndexedBlock) -> Result<(), String> {
+        let next_maintenance_time = self
+            .manager
+            .state_db
+            .must_get(&keys::DynamicProperty::NextMaintenanceTime);
+
+        let is_maintenance = next_maintenance_time <= block.timestamp();
+
+        if is_maintenance {
+            if block.number() != 1 {
+                self.do_maintenance(block)?;
+            }
+            // updateNextMaintenanceTime
+            self.increase_next_maintenance_time(next_maintenance_time, block.timestamp())?;
+        }
+        self.manager
+            .state_db
+            .put_key(keys::DynamicProperty::IsMaintenance, is_maintenance as _)
+            .unwrap();
+        Ok(())
+    }
+
+    fn do_maintenance(&mut self, block: &IndexedBlock) -> Result<(), String> {
+        // 0: default
+        // 1: remove now
+        // -1: removed
+        if self.manager.state_db.must_get(&keys::ChainParameter::RemovePowerOfGr) == 1 {
+            self.remove_power_of_gr()?;
+        }
+
+        let _votes = self.count_votes();
+
+        // TODO: handle vote
+        // unimplemented!()
+        debug!("TODO: handle do_maintenance(votes counting) at block #{}", block.number());
+        Ok(())
+    }
+
+    fn count_votes(&self) -> Vec<(Address, i64)> {
+        use std::collections::HashMap;
+
+        let mut votes: HashMap<Address, i64> = HashMap::new();
+
+        // value is a &state_pb::Votes
+        {
+            let votes = &mut votes;
+            self.manager.state_db.for_each(move |key: &keys::Votes, value| {
+                debug!("got votes key => {:?} {:?}", key, value);
+                for vote in value.votes.iter() {
+                    *votes.entry(*Address::from_bytes(&vote.vote_address)).or_default() += vote.vote_count;
+                }
+            });
+        }
+        // TODO: unimplemented!()
+        vec![]
+    }
+
+    // in DynamicPropertiesStore.java
+    fn increase_next_maintenance_time(
+        &mut self,
+        current_next_maintenance_time: i64,
+        block_ts: i64,
+    ) -> Result<(), String> {
+        let maintenance_interval = self
+            .manager
+            .state_db
+            .must_get(&keys::ChainParameter::MaintenanceInterval);
+
+        let round = (block_ts - current_next_maintenance_time) / maintenance_interval;
+        let next_maintenance_time = current_next_maintenance_time + (round + 1) * maintenance_interval;
+        self.manager
+            .state_db
+            .put_key(keys::DynamicProperty::NextMaintenanceTime, next_maintenance_time)
+            .unwrap();
+        Ok(())
+    }
+    fn remove_power_of_gr(&mut self) -> Result<(), String> {
+        unimplemented!()
+    }
+}
