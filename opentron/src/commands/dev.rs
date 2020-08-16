fd07d13d3a9e97d6a814f2fc1512d393ba3cd816
andelf <andelf@gmail.com>
2020-08-16 18:17:12 +0800

    fix(node): now dev command run through all blocks

--- opentron/src/commands/dev.rs
+++ opentron/src/commands/dev.rs
@@ -24,8 +24,8 @@ pub async fn main<P: AsRef<Path>>(config_path: P, _matches: &ArgMatches<'_>) -> 
 
     let start_block = db_manager.latest_block_number() as u64 + 1;
 
-    for i in start_block..30000 {
-        //for i in 28514..30000 {
+    // 741457, first AssetIssueContract
+    for i in start_block.. {
         let blk = ctx.chain_db.get_block_by_number(i)?;
 
         db_manager.push_block(&blk)?;
