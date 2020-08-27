d8dcee940c42a1f17210662fd9a5b595f6c83af5
andelf <andelf@gmail.com>
2020-08-28 03:30:24 +0800

    feat(tvm): add tvm

---
+++ tvm/src/backend.rs
@@ -0,0 +1,1 @@
+pub use evm::backend::{Apply, ApplyBackend, Backend, Basic, Log};
---
+++ tvm/src/lib.rs
@@ -0,0 +1,4 @@
+pub use evm::executor::StackExecutor;
+pub use evm::{Config, Context, Runtime};
+
+pub mod backend;
