0c6a4b9beb45ef8e9323bd1cb48f72e2f254eef9
Andelf <andelf@gmail.com>
2021-03-02 04:18:33 +0800

    feat(chain): add BlockBuilder

---
+++ chain/src/block_builder.rs
@@ -0,0 +1,58 @@
+use crate::{BlockHeader, IndexedBlock, Transaction};
+use keys::{Address, KeyPair};
+use primitive_types::H256;
+use prost::Message;
+
+pub struct BlockBuilder {
+    header: BlockHeader,
+    txns: Vec<Transaction>,
+}
+
+impl BlockBuilder {
+    pub fn new(number: i64) -> Self {
+        let mut header = BlockHeader::default();
+        header.raw_data = Some(Default::default());
+        header.raw_data.as_mut().map(|h| {
+            h.number = number;
+        });
+        BlockBuilder { header, txns: vec![] }
+    }
+
+    pub fn timestamp(mut self, timestamp: i64) -> Self {
+        self.header.raw_data.as_mut().map(|h| h.timestamp = timestamp);
+        self
+    }
+
+    pub fn version(mut self, version: i32) -> Self {
+        self.header.raw_data.as_mut().map(|h| h.version = version);
+        self
+    }
+
+    pub fn parent_hash(mut self, hash: &H256) -> Self {
+        self.header
+            .raw_data
+            .as_mut()
+            .map(|h| h.parent_hash = hash.as_bytes().into());
+        self
+    }
+
+    pub fn push_transaction(mut self, txn: Transaction) -> Self {
+        self.txns.push(txn);
+        self
+    }
+
+    pub fn build(mut self, witness: &Address, keypair: &KeyPair) -> Option<IndexedBlock> {
+        self.header
+            .raw_data
+            .as_mut()
+            .map(|h| h.witness_address = witness.as_bytes().into());
+
+        let mut buf = Vec::with_capacity(255);
+        self.header.raw_data.as_ref().unwrap().encode(&mut buf).unwrap();
+
+        let signature = keypair.private().sign(&buf).ok()?;
+        self.header.witness_signature = signature.as_bytes().into();
+
+        IndexedBlock::from_raw_header_and_txns(self.header, self.txns)
+    }
+}
