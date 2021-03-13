d9fbcfa2a6e3425e6a54856c1444b1cc79c4c8f2
Andelf <andelf@gmail.com>
2021-03-14 06:20:10 +0800

    feat(cli): add simple cli to send txns

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,41 +1,108 @@
 use chrono::Utc;
+use clap::ArgMatches;
 use crypto::sha256;
 use keys::{Address, KeyPair};
 use prost::Message;
-use proto::chain::transaction::Raw as TransactionRaw;
+use proto::chain::transaction::{Contract, Raw as TransactionRaw};
 use proto::contract as contract_pb;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let from: Address = "TJrsCAaTePcnB2UWkqA53nF2dnyVovotrx".parse()?;
-    let kp = KeyPair::from_private("d9c97ae310817434c1901b7eba568ed04c52ccc14cf365f52c78f08664a8e63b".parse()?)?;
-
-    let to: Address = "TJRabPrwbZy45sbavfcjinPJC18kjpRTv8".parse()?;
-
-    let ref_block_hash = hex::decode("000000000002927b159add8337b8beaf8ac82a9635dacbab2c1aaef90e3fd5d7")?;
+fn transfer(matches: &ArgMatches) -> Option<Contract> {
+    let from: Address = matches.value_of("SENDER")?.parse().ok()?;
+    let to: Address = matches.value_of("RECIPIENT")?.parse().ok()?;
+    let amount = matches.value_of("AMOUNT")?.parse().ok()?;
 
     let transfer = contract_pb::TransferContract {
         owner_address: from.as_bytes().to_vec(),
         to_address: to.as_bytes().to_vec(),
-        amount: 10_000_000,
+        amount: amount,
     };
 
+    Some(transfer.into())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let yaml = clap::load_yaml!("cli.yml");
+    let matches = clap::App::from_yaml(yaml).get_matches();
+
+    let cntr = match matches.subcommand() {
+        ("transfer", Some(arg_matches)) => transfer(arg_matches),
+        // commands::transfer::main(arg_matches),
+        // ("list", Some(arg_matches)) => commands::list::main(arg_matches),
+        _ => unimplemented!(),
+    };
+
+    pack_and_send(cntr.unwrap(), &matches)?;
+
+    Ok(())
+}
+
+fn pack_and_send(cntr: Contract, matches: &ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
+    let ref_block_hash = get_ref_block_hash()?;
+
     let raw = TransactionRaw {
-        contract: Some(transfer.into()),
+        contract: Some(cntr),
         ref_block_bytes: ref_block_hash[6..8].to_vec(),
         ref_block_hash: ref_block_hash[8..16].to_vec(),
         expiration: Utc::now().timestamp_millis() + 60_000,
         ..Default::default()
     };
     let mut buf = Vec::with_capacity(255);
-
     raw.encode(&mut buf)?;
 
-    println!("=> {}", hex::encode(&buf));
-    println!("=> size {}", buf.len());
-    println!("=> txn hash {:?}", sha256(&buf));
+    let hex_priv_key = matches.value_of("private-key").unwrap();
+    let kp = KeyPair::from_private(hex_priv_key.parse()?)?;
 
     let sig = kp.private().sign(&buf)?;
-    println!("=> sig {}", hex::encode(sig.as_bytes()));
+
+    println!("RAW => {}", hex::encode(&buf));
+    println!("TXN Hash => {:?}", sha256(&buf));
+    println!("SIG => {}", hex::encode(sig.as_bytes()));
+
+    send_raw_transaction(&hex::encode(&buf), &hex::encode(sig.as_bytes()))?;
 
     Ok(())
+}
+
+fn send_raw_transaction(raw: &str, signature: &str) -> Result<(), Box<dyn Error>> {
+    let client = reqwest::blocking::Client::new();
+    let mutation = r#"{ "query": "mutation { txn: sendRawTransaction(rawData: \"RAW\", signatures: [\"SIG\"]) }" }"#;
+
+    let mutation = mutation.replace("RAW", raw).replace("SIG", signature);
+    let resp = client
+        .post("http://localhost:3000/")
+        .header("User-Agent", "Opentron Cli/0.1.0")
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .body(mutation)
+        .send()?;
+
+    let payload: serde_json::Value = resp.json()?;
+
+    println!("{}", serde_json::to_string_pretty(&payload)?);
+    let hash = &payload["data"]["txn"];
+    println!("=> {}", hash);
+    Ok(())
+}
+
+fn get_ref_block_hash() -> Result<Vec<u8>, Box<dyn Error>> {
+    let client = reqwest::blocking::Client::new();
+    // "operationName":null,
+    // "variables":{},
+    let query = r#"{
+        "query":"{ refBlock: block { hash } }"
+    }"#;
+    let resp = client
+        .post("http://localhost:3000/")
+        .header("User-Agent", "Opentron Cli/0.1.0")
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .body(query)
+        .send()?;
+
+    let payload: serde_json::Value = resp.json()?;
+
+    println!("{}", serde_json::to_string_pretty(&payload)?);
+    let hash = &payload["data"]["refBlock"]["hash"];
+    hex::decode(hash.as_str().unwrap()).map_err(From::from)
 }
